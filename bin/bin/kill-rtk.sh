db329567199d9cf1c9eef0f3ee2d6d77fc6f7e5f
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-20 08:46:25 +0300

    Add scripts for starting and killing work apps

---
+++ bin/bin/kill-rtk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+snxctl disconnect
+killall remmina
+killall snx-rs-gui
+killall vivaldi-bin
+pkill -o yuchat.bin
+killall TrueConf
---
+++ bin/bin/start-rtk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+vivaldi &
+sleep 1
+yuchat &
+sleep 1
+trueconf &
+sleep 1
+snx-rs-gui &
+remmina -i &
