f342499d728832d51ddabbd4db999298ded93933
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-21 17:40:45 +0300

    Start-rtk changes

--- bin/bin/start-rtk.sh
+++ bin/bin/start-rtk.sh
@@ -6,5 +6,7 @@ yuchat &
 sleep 1
 trueconf &
 sleep 1
-snx-rs-gui &
+snx-rs-gui -m connect &
 remmina -i &
+sleep 3
+niri msg action focus-window --id $(niri msg --json windows | jq -r '.[] | select(.app_id == "snx-rs-gui") | select(.title="VPN Authentication Factor") | .id')
