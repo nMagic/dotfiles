b944d624a92e3fcb77e4a9aa11ba3c131550bfd6
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-26 22:23:43 +0300

    Tune start-rtk

--- bin/bin/start-rtk.sh
+++ bin/bin/start-rtk.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+snx-rs-gui -m connect &
+niri msg action focus-window --id "$(niri msg --json windows | jq -r '.[] | select(.app_id == "snx-rs-gui") | select(.title="VPN Authentication Factor") | .id')"
+
+while [[ $(snxctl status | grep -c "Connected") -eq 0 ]]; do
+  sleep 1
+done
+
 vivaldi &
 sleep 1
 yuchat &
 sleep 1
 trueconf &
-sleep 1
-snx-rs-gui -m connect &
 remmina -i &
-sleep 3
-niri msg action focus-window --id $(niri msg --json windows | jq -r '.[] | select(.app_id == "snx-rs-gui") | select(.title="VPN Authentication Factor") | .id')
