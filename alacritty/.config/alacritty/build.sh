2673a1e6a0ca4c58f8ed45f782808b78c84853c3
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-30 14:10:00 +0300

    [alacritty] Use smartstow now

--- alacritty/.config/alacritty/build.sh
+++
@@ -1,4 +0,0 @@
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cp "$SCRIPTPATH/base.toml" "$SCRIPTPATH/alacritty.toml"
-mkdir "$HOME/.config/alacritty" 2>/dev/null
-git clone https://github.com/alacritty/alacritty-theme "$HOME/.config/alacritty/themes-from-git"
