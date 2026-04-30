79aa0174d7c5e8a8b5dc1dc3c9158d81cac0f56d
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-30 13:45:08 +0300

    [RTK] Add workspace logic

--- bin/bin/kill-rtk.sh
+++ bin/bin/kill-rtk.sh
@@ -6,3 +6,7 @@ killall snx-rs-gui
 killall vivaldi-bin
 pkill -o yuchat.bin
 killall TrueConf
+
+if [[ "$(cat /etc/hostname)" == "DmitriyPC" ]]; then
+  niri msg action unset-workspace-name 󰦑
+fi
--- bin/bin/start-rtk.sh
+++ bin/bin/start-rtk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+workspace_id="$(niri msg --json workspaces | jq -r '.[] | select(.name == "󰦑") | .idx')"
+if [[ -z $workspace_id ]]; then
+  workspace_id="$(niri msg --json workspaces | jq -r '.[].idx' | sort -n | tail -1)"
+fi
+
+niri msg action focus-workspace "$workspace_id" && niri msg action set-workspace-name 󰦑
+
 snx-rs-gui -m connect &
 niri msg action focus-window --id "$(niri msg --json windows | jq -r '.[] | select(.app_id == "snx-rs-gui") | select(.title="VPN Authentication Factor") | .id')"
 
