aeaacf1cd1f5bde39998914bd2c126dc836c7f46
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-04-30 13:44:23 +0300

    [wlr-which-key] Use smartstow now

--- wlr-which-key/.smartstow/src/set-theme.sh
+++ wlr-which-key/.smartstow/src/set-theme.sh
@@ -4,4 +4,5 @@ CONFIG="$HOME/.config/wlr-which-key/config.yaml"
 THEME="$HOME/.config/wlr-which-key/theme.yaml"
 
 sed -E -i --follow-symlinks '/^(background:|color:|border:)/d' "$CONFIG"
-cat $THEME <(echo) $CONFIG > $CONFIG'.tmp' && cat $CONFIG'.tmp' > $CONFIG && rm $CONFIG'.tmp' 
+cat "$THEME" <(echo) "$CONFIG" >"$CONFIG"'.tmp' && cat "$CONFIG"'.tmp' >"$CONFIG" && rm "$CONFIG"'.tmp'
+
