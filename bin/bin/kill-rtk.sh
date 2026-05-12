b2de0d53315446984ead46e9c281aba9f8923bd4
Dmitriy Glebenkov <dvg1995@gmail.com>
2026-05-12 11:27:06 +0300

    [RTK] Add more intelligent sleeps

--- bin/bin/kill-rtk.sh
+++ bin/bin/kill-rtk.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
 snxctl disconnect
+while [[ $(snxctl status) != "Disconnected" ]]; do
+  sleep 0.1
+  echo "snx disconnecting"
+done
+snx-rs-gui -m exit
+while [[ ! $(pgrep -c snx-rs-gui) -eq 0 ]]; do
+  sleep 0.1
+  echo "Exiting snx gui"
+done
 killall remmina
-killall snx-rs-gui
+while [[ ! $(pgrep -c remmina) -eq 0 ]]; do
+  sleep 0.1
+  echo "Exiting remmina"
+done
 killall vivaldi-bin
-pkill -o yuchat.bin
+while [[ ! $(pgrep -c vivaldi-bin) -eq 0 ]]; do
+  sleep 0.1
+  echo "Exiting vivaldi"
+done
 killall TrueConf
+while [[ ! $(pgrep -c TrueConf) -eq 0 ]]; do
+  sleep 0.1
+  echo "Exiting trueconf"
+done
+pkill -o yuchat.bin
+while [[ ! $(pgrep -c yuchat) -eq 0 ]]; do
+  sleep 0.1
+  echo "Exiting yuchat"
+done
 
 if [[ "$(cat /etc/hostname)" == "DmitriyPC" ]]; then
   niri msg action unset-workspace-name 󰦑
--- bin/bin/start-rtk.sh
+++ bin/bin/start-rtk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+current_window="$(niri msg --json focused-window | jq -r '.id')"
 workspace_id="$(niri msg --json workspaces | jq -r '.[] | select(.name == "󰦑") | .idx')"
 if [[ -z $workspace_id ]]; then
   workspace_id="$(niri msg --json workspaces | jq -r '.[].idx' | sort -n | tail -1)"
@@ -8,15 +9,40 @@ fi
 niri msg action focus-workspace "$workspace_id" && niri msg action set-workspace-name 󰦑
 
 snx-rs-gui -m connect &
-niri msg action focus-window --id "$(niri msg --json windows | jq -r '.[] | select(.app_id == "snx-rs-gui") | select(.title="VPN Authentication Factor") | .id')"
-
-while [[ $(snxctl status | grep -c "Connected") -eq 0 ]]; do
-  sleep 1
+while [[ $(snxctl status) == "Disconnected" ]]; do
+  sleep 0.1
+  echo disconnected
+done
+while [[ $(snxctl status) == "Connecting in progress" ]]; do
+  sleep 0.1
+  echo connecting
 done
 
+while [[ $(snxctl status) == "MFA pending: PasswordInput" ]]; do
+  sleep 1
+  echo pending
+done
+if [[ $(snxctl status) == "Disconnected" ]]; then
+  echo disconnected
+  niri msg action focus-window --id "$current_window"
+  niri msg action unset-workspace-name 󰦑
+  snx-rs-gui -m exit
+  exit 0
+fi
+
 vivaldi &
-sleep 1
+while [[ "$(niri msg --json focused-window | jq -r '.app_id')" != "vivaldi-stable" ]]; do
+  echo "launching vivaldi"
+  sleep 0.1
+done
 yuchat &
-sleep 1
+while [[ "$(niri msg --json focused-window | jq -r '.app_id')" != "YuChat" ]]; do
+  echo "launching yuchat"
+  sleep 0.1
+done
 trueconf &
+while [[ "$(niri msg --json focused-window | jq -r '.app_id')" != "TrueConf" ]]; do
+  echo "launching trueconf"
+  sleep 0.1
+done
 remmina -i &
